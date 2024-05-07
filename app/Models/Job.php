b5cbc962612b7f691c2885cc086fd3e343ddab49
noblica06 <noblica06@gmail.com>
2024-05-07 13:38:08 +0200

    Started Eloquent ORM

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,35 +1,11 @@
 <?php
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
 
-class Job {
-    public static function all() : array{
-        return [
-                [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-                ],
-                [
-                    'id' => 2,
-                    'title' => 'Programmer',
-                    'salary' => '$10,000'
-                ],
-                [
-                    'id' => 3,
-                    'title' => 'Teacher',
-                    'salary' => '$40,000'
-                ],
-            ];
-    }
-
-    public static function find($id): array{
-        $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
-        if(! $job){
-            abort(404);
-        }
-        return $job;
-    }
+class Job extends Model {
+   protected $table = 'job_listings'; 
+   protected $fillable = ['title', 'salary'];
 }
