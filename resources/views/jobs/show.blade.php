e8f5631465ebef653afcd49d01f604be83ce1f53
noblica06 <noblica06@gmail.com>
2024-05-08 09:24:34 +0200

    completed simple CRUD operations with patch/delete

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -2,7 +2,12 @@
     <x-slot:heading>
         Job
     </x-slot:heading>
-    <h2 class="font-bold test-lg">{{$job['title']}}</h2>
+    <h2 class="font-bold test-lg">{{$job->title}}</h2>
 
-    <p>This job pays {{$job['salary']}} per year</p>
+    <p>This job pays {{$job->salary}} per year</p>
+    <p class="mt-6">
+        <x-button href="/jobs/{{$job->id}}/edit">
+            Edit Job
+        </x-button>
+    </p>
 </x-layout>
